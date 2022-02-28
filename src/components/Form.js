b94707b6aef246ed5a888bb79b3e8bdee8df607c
David Soto <davidsotosendarrubias@gmail.com>
2022-02-28 03:26:35 +0100

    added form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,30 @@
+import FormStyle from "../style/formStyle";
+
 const SignIn = () => {
   return (
     <>
-      <h2>Sign In</h2>
-      <label htmlFor="">UserName</label>
-      <input type="text" />
+      <FormStyle>
+        <h2 className="title">Sign In </h2>
+        <label htmlFor="">UserName</label>
+        <input type="text" />
 
-      <label htmlFor="">Name</label>
-      <input type="text" />
+        <label htmlFor="">Name</label>
+        <input type="text" />
 
-      <label htmlFor="">Surname</label>
-      <input type="text" />
+        <label htmlFor="">Surname</label>
+        <input type="text" />
 
-      <label htmlFor="">Age</label>
-      <input type="number" />
+        <label htmlFor="">Age</label>
+        <input type="number" />
 
-      <label htmlFor="">Email</label>
-      <input type="text" />
+        <label htmlFor="">Email</label>
+        <input type="text" />
 
-      <label htmlFor="">Password</label>
-      <input type="text" />
+        <label htmlFor="">Password</label>
+        <input type="text" />
+
+        <button>Sign In</button>
+      </FormStyle>
     </>
   );
 };
